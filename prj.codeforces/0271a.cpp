c4e755790d98624dd38dbaa9288654b1eecdf055
Srg206 <145347125+Srg206@users.noreply.github.com>
2023-09-18 20:50:15 +0300

    Create 0271a.cpp

---
+++ prj.codeforces/0271a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <vector>	
+
+int main()
+{
+	int y = 0;
+	std::cin >> y;
+	for (int i = y+1; ; i++) {
+		int a, b, c, d;
+		a = i % 10;
+		b = (i / 10) % 10;
+		c = (i / 100) % 10;
+		d = (i / 1000);
+		if (a != b && a != c && a != d && b != c && b != d && c != d) {
+			std::cout << i;
+			break;
+		}
+
+
+	}
+	
+
+	return 0;
+}

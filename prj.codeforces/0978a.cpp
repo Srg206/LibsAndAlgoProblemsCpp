1ab3ebc0dc1ae8e55ed676c0db877d4be6adfbaa
Srg206 <145347125+Srg206@users.noreply.github.com>
2023-09-18 20:35:28 +0300

    Create 0978a.cpp

---
+++ prj.codeforces/0978a.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <list>
+#include <vector>
+
+
+int main()
+{
+	std::list<int> Arr;
+	std::list<int> FinishArr;
+	int _size = 0;
+	std::cin >> _size;
+	for (int i = 0; i < _size; i++)
+	{
+		int a = 0;
+		std::cin >> a;
+		Arr.push_front(a);
+	}
+	std::list<int>::iterator it;
+	std::list<int>::iterator i;
+	for (it = Arr.begin(); it != Arr.end(); ++it) {
+		int kolvo_finish = count(FinishArr.begin(), FinishArr.end(), *it);
+		if (kolvo_finish == 0)
+		{
+			FinishArr.push_front(*it);
+		}
+	}	
+	std::cout<<FinishArr.size() <<std::endl;
+	for (i = FinishArr.begin(); i != FinishArr.end(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+
+
+	return 0;
+}

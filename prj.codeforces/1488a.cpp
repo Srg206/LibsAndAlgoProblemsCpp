f328ae70e7d617b24c9ac0cd6f1c79eaf729f0aa
Srg206 <145347125+Srg206@users.noreply.github.com>
2023-09-22 09:23:12 +0300

    Update 1488a.cpp

--- prj.codeforces/1488a.cpp
+++ prj.codeforces/1488a.cpp
@@ -10,33 +10,26 @@ int main()
 	int t = 0;
 	std::cin >> t;
 
-	for (int i = 0; i < t; i++)
-	{
-		int Amount_Operations=0;
+	for (int i = 0; i < t; i++){
+		int amount_of_operations = 0;
 		std::cin >> x;
 		std::cin >> y;
 		int tmp = y;
-		for (; (tmp % x) != 0; tmp--,Amount_Operations++)
-		{
+		for (; (tmp % x) != 0; tmp--, amount_of_operations++){
 
 		}
-		int Max_Decrease=x;
-		while (Max_Decrease*10 < tmp)
-		{
-			Max_Decrease = Max_Decrease * 10;
+		int max_decrease = x;
+		while (max_decrease * 10 < tmp){
+			max_decrease = max_decrease * 10;
 		}
-		//std::cout<< Max_Decrease <<std::endl;
-		for (int i = Max_Decrease; i >= x; i /= 10)
-		{
-	//	std::cout<< i <<std::endl;
-			while (tmp>=i)
-			{
+		for (int i = max_decrease; i >= x; i /= 10){
+			while (tmp >= i){
 				tmp -= i;
-				Amount_Operations++;
+				amount_of_operations++;
 			}
 		}
 
-		std::cout << Amount_Operations <<std::endl;
+		std::cout << amount_of_operations << std::endl;
 
 	}
 	return 0;

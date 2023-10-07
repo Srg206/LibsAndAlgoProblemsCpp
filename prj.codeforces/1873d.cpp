295b2e4c0aff5e4bb36eeb54f435ea81cb93f4c3
Srg206 <Srg206.32@gmail.com>
2023-10-07 16:22:03 +0300

    update

---
+++ prj.codeforces/1873d.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<array>
+#include<cmath>
+#include<string>
+#include<algorithm>
+
+int main()
+{
+
+	int amount_of_data = 0;
+	int len = 0;
+	int together_elements = 0;
+	std::string str;
+	std::vector<int> results;
+	std::cin >> amount_of_data;
+	for (int i = 0; i < amount_of_data; i++) {
+		std::cin >> len >> together_elements >> str;
+
+		int counter = 0;
+		for (int j = 0; j < len; j++) {
+			if (str[j] == 'B') {
+				for (int k = j; k < j + together_elements && k<len; k++) {
+					str[k] = 'W';
+				}
+				counter += 1;
+				j += together_elements-1;
+			}
+		}
+		results.push_back(counter);
+
+	}
+
+
+	for (int i = 0; i < amount_of_data; i++) {
+		std::cout << results[i]<<std::endl;
+	}
+
+
+	return 0;
+}

c43fd098c3778bba0bb0a53596304e6461a5e104
Srg206 <Srg206.32@gmail.com>
2023-10-09 13:03:58 +0300

    udp

---
+++ prj.codeforces/0041a.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<array>
+#include<cmath>
+#include<string>
+#include<algorithm>
+
+int main()
+{
+	std::string ber_str;
+	std::string bir_str;
+
+	std::string result="YES";
+	std::cin >> ber_str >> bir_str;
+	if (ber_str.size() != bir_str.size()) {
+		result = "NO";
+	}
+	else {
+		for (int i = 0; i < ber_str.size(); i++) {
+			if (ber_str[i] != bir_str[bir_str.size() - 1 - i]) {
+				result = "NO";
+				break;
+			}
+		}
+	}
+	std::cout << result;
+
+	return 0;
+}

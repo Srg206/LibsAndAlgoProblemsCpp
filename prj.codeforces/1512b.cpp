92cf8151968954b86d9284a2569e4cfe25d49d77
Srg206 <Srg206.32@gmail.com>
2023-10-20 09:53:29 +0300

    udp

---
+++ prj.codeforces/1512b.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<vector>
+#include<array>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<limits.h>
+
+
+int main()
+{
+	int x1 = 0;
+	int x2 = 0;
+	int y1 = 0;
+	int y2 = 0;
+	int amount_of_data = 0;
+	std::vector<std::vector<std::string>> results;
+	std::vector<int> len;
+	std::cin >> amount_of_data;
+	for (int i = 0; i < amount_of_data; i++) {
+		int matrix_size = 0;
+		std::cin >> matrix_size;
+		std::vector<std::string> matrix;
+		for (int j = 0; j < matrix_size; j++) {
+			//getline(std::cin,matrix[j]);
+			std::string s;
+			std::cin >> s;
+			matrix.push_back(s);
+		}
+
+
+		int dot_counter = 0;
+		for (int j = 0; j < matrix_size; j++) {
+			for (int k = 0; k < matrix_size; k++) {
+				if (matrix[j][k] == '*' && dot_counter == 0) {
+					x1 = k;
+					y1 = j;
+				}
+				if (matrix[j][k] == '*' && dot_counter == 1) {
+					x2 = k;
+					y2 = j;
+				}
+			}
+		}
+
+
+
+		if (x1 ==x2) {
+			matrix[y1][(x1+1)%matrix_size] = '*';
+			matrix[y2][(x1 + 1) % matrix_size] = '*';
+		}
+		if (y1 == y2) {
+			matrix[(y1+1)%matrix_size][x1] = '*';
+			matrix[(y2+1)%matrix_size][x2] = '*';
+		}
+		if (x1 > x2 && y1 < y2) {
+			matrix[y1][x2] = '*';
+			matrix[y2][x1] = '*';
+		}
+		if (x1 < x2 && y1 > y2) {
+			matrix[y1][x2] = '*';
+			matrix[y2][x1] = '*';
+		}
+
+
+		results.push_back(matrix);
+		len.push_back(matrix_size);
+	}
+
+	for (int i = 0; i < amount_of_data; i++) {
+		for (int j = 0; j < len[i]; j++) {
+			for (int k = 0; k < len[i]; k++) {
+				std::cout << results[i][j][k];
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	
+
+	return 0;
+}
